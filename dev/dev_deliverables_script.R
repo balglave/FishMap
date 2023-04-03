ebda09d9ad5bc866bb9d101d41d6bf01fc7388d9
cervan <cervan@thinkr.Fr>
2023-04-03 09:25:37 +0000

    [fix] Install deps for reports
    why
    
    - covr and testdown work
    
    how :
    
    - add dep to true in install.packages
    
    #82

--- dev/dev_deliverables_script.R
+++ dev/dev_deliverables_script.R
@@ -1,8 +1,8 @@
 unlink("pkgdown/assets", recursive = TRUE)
 dir.create("pkgdown/assets", recursive = TRUE)
 
-if (!requireNamespace("covr", quietly = TRUE)) install.packages("covr")
-if (!requireNamespace("testdown", quietly = TRUE)) install.packages("testdown", repos = c("thinkropen" = "https://thinkr-open.r-universe.dev"))
+if (!requireNamespace("covr", quietly = TRUE)) install.packages("covr", dependencies = TRUE)
+if (!requireNamespace("testdown", quietly = TRUE)) install.packages("testdown", repos = c("thinkropen" = "https://thinkr-open.r-universe.dev"), dependencies = TRUE)
 
 # Create covr book and add it to pkgdown ----
 covr::report(file = "pkgdown/assets/coverage.html", browse = FALSE)
@@ -13,4 +13,4 @@ testdown::test_down(open = FALSE)
 file.copy("tests/testdown", "pkgdown/assets", recursive = TRUE)
 unlink("tests/testdown", recursive = TRUE)
 
-pkgdown::build_site()
+# pkgdown::build_site()
