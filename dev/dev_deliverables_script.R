24ed5ad6976a671c77c87bb21d742df1593c5fff
cervan <cervan@thinkr.Fr>
2023-04-03 11:01:50 +0000

    [fix]: add cran repo also
    why :
    
    - try to install dependecies on cran

--- dev/dev_deliverables_script.R
+++ dev/dev_deliverables_script.R
@@ -2,8 +2,7 @@ unlink("pkgdown/assets", recursive = TRUE)
 dir.create("pkgdown/assets", recursive = TRUE)
 
 if (!requireNamespace("covr", quietly = TRUE)) install.packages("covr", dependencies = TRUE)
-if (!requireNamespace("remotes", quietly = TRUE)) install.packages("remotes")
-if (!requireNamespace("testdown", quietly = TRUE)) remotes::install_github("ThinkR-open/testdown", dependencies = TRUE)
+if (!requireNamespace("testdown", quietly = TRUE)) install.packages('testdown', repos = c('https://thinkr-open.r-universe.dev', 'https://cloud.r-project.org'))
 
 # Create covr book and add it to pkgdown ----
 covr::report(file = "pkgdown/assets/coverage.html", browse = FALSE)
